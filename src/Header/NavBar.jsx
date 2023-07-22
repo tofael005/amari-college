583e324d5eca7cc5a5a70540e777ec92ee7e8a9e
tofae <arnobtuhin,j@gmail.com>
2023-07-22 11:13:19 +0600

    navbar added

--- src/Header/NavBar.jsx
+++ src/Header/NavBar.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { AiOutlineClose } from 'react-icons/ai';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const NavBar = () => {
+    const [toggle, setToggle] = useState()
     return (
         <div className="bg-orange-300">
            <div className="max-w-[1240px] mx-auto flex justify-between items-center p-4">
@@ -8,8 +12,13 @@ const NavBar = () => {
                 <h1>LOGO</h1>
             </div>
             
+            <span className='md:hidden'>
+                {
+                    toggle ? <AiOutlineClose className='text-2xl' onClick={() => setToggle(!toggle)} /> :  <GiHamburgerMenu className="text-2xl" onClick={() => setToggle(!toggle)} />
+                }
+            </span>
 
-            <ul className="flex items-center gap-6">
+            <ul className={`flex items-center duration-300 flex-col py-5 md:p-0 text-white md:text-gray-900 bg-opacity-90 top-14 bg-black md:bg-transparent w-full text-center md:w-auto md:flex-row gap-2 absolute md:static z-50 ${toggle ? "left-0" : "-left-full"}`}>
                 <li>
                     <NavLink to="/">Home</NavLink>
                 </li>
