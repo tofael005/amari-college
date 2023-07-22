9d990aebea1d06862785ffd1dfda59d55a08bc79
tofae <arnobtuhin,j@gmail.com>
2023-07-22 20:50:31 +0600

    nav update

--- src/Header/NavBar.jsx
+++ src/Header/NavBar.jsx
@@ -20,20 +20,20 @@ const NavBar = () => {
                     }
                 </span>
 
-                <ul className={`flex items-center duration-300 flex-col py-5 md:p-0 text-white md:text-gray-900 bg-opacity-90 top-14 bg-black md:bg-transparent w-full text-center md:w-auto md:flex-row gap-6 absolute md:static z-50 ${toggle ? "left-0" : "-left-full"}`}>
-                    <li>
+                <ul className={`flex items-center duration-300 flex-col py-5 md:p-0 text-white md:text-gray-900 bg-opacity-90 top-14 bg-black md:bg-transparent w-full text-center md:w-auto md:flex-row gap-4 absolute md:static z-50 ${toggle ? "left-0" : "-left-full"}`}>
+                    <li className="hover:bg-[#05b6d1] duration-300 px-3 py-2 rounded-md hover:text-white font-semibold">
                         <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink>
                     </li>
-                    <li>
+                    <li className="hover:bg-[#05b6d1] duration-300 px-3 py-2 rounded-md hover:text-white font-semibold">
                         <NavLink to="/college" className={({ isActive }) => isActive ? "active" : ""}>College</NavLink>
                     </li>
-                    <li>
+                    <li className="hover:bg-[#05b6d1] duration-300 px-3 py-2 rounded-md hover:text-white font-semibold">
                         <NavLink to="/admission" className={({ isActive }) => isActive ? "active" : ""}>Admission</NavLink>
                     </li>
-                    <li>
+                    <li className="hover:bg-[#05b6d1] duration-300 px-3 py-2 rounded-md hover:text-white font-semibold">
                         <NavLink to="/myCollege" className={({ isActive }) => isActive ? "active:" : ""}>My-College</NavLink>
                     </li>
-                    <li>
+                    <li className="hover:bg-[#05b6d1] duration-300 px-3 py-2 rounded-md hover:text-white font-semibold">
                         <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>Blog</NavLink>
                     </li>
                 </ul>
